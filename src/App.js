86ff2316a647d5445ee07b6c9505cde498df6e80
Manjula <mguneratne3@gmail.com>
2022-06-15 16:52:35 -0400

    use State added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const Notes = props => props.data.map(note => <div>{note.text}</div>);
+
+  const initialDate = [{ text: 'Hey'}, {text: 'There'}];
+  const [data, setData] = useState(initialDate);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
