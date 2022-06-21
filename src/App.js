e45400a267dafcbe6e5b06dd789bff49eb226ad1
Manjula <mguneratne3@gmail.com>
2022-06-20 22:23:46 -0400

    just started added useEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import produce from 'immer';
 
 function App() {
 
   const Notes = props => props.data.map(note => <div>{note.text}</div>);
 
-  const initialDate = [{ text: 'Hey'}, {text: 'There'}];
+  const initialDate = [{ text: 'Loading notes ...'}];
   const [data, setData] = useState(initialDate);
 
+  useEffect(() => {
+    if(typeof window !== 'undefined'){
+      const getData = localStorage.getItem('data');
+      if(getData !== '' && getData !== null){
+        return setData(JSON.parse(getData));
+      }
+      return setData([]);
+    }
+  },0);
+
+  const handleClick = () => {
+    const text = document.querySelector('#noteinput').value.trim();
+    if(text){
+      const nextState = produce(data, draftState => {
+        draftState.push({text});
+      });
+      document.querySelector('#noteinput').value = '';
+      setData(nextState);
+    }
+  };
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <input style={{width: '80%'}} type="text" placeholder='Enter a new note' />
+
+      <button onClick={() => handleClick()}>Add note</button>
+
+      <Notes data={data} />      
+    </>
   );
 }
 
