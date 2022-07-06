a15d463812245ac9b0bc65585ecaa45ee6e05512
Manjula <mguneratne3@gmail.com>
2022-07-06 16:18:37 -0400

    Added the note ID and move notes const as a global var

--- src/App.js
+++ src/App.js
@@ -1,42 +1,46 @@
-import './App.css';
-import {useState, useEffect} from 'react';
-import produce from 'immer';
+import "./App.css";
+import { useState, useEffect } from "react";
+import produce from "immer";
+
+const Notes = (props) => props.data.map((note) => <div>{note.text}</div>);
 
 function App() {
-
-  const Notes = props => props.data.map(note => <div>{note.text}</div>);
-
-  const initialDate = [{ text: 'Loading notes ...'}];
+  const initialDate = [{ text: "Loading notes ..." }];
   const [data, setData] = useState(initialDate);
 
-  useEffect(() => {
-    if(typeof window !== 'undefined'){
-      const getData = localStorage.getItem('data');
-      if(getData !== '' && getData !== null){
-        return setData(JSON.parse(getData));
-      }
-      return setData([]);
-    }
-  },0);
-
   const handleClick = () => {
-    const text = document.querySelector('#noteinput').value.trim();
-    if(text){
-      const nextState = produce(data, draftState => {
-        draftState.push({text});
+    const text = document.querySelector("#noteinput").value.trim();
+    if (text) {
+      const nextState = produce(data, (draftState) => {
+        draftState.push({ text });
       });
-      document.querySelector('#noteinput').value = '';
+      document.querySelector("#noteinput").value = "";
       setData(nextState);
     }
   };
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const getData = localStorage.getItem("data");
+      if (getData !== "" && getData !== null) {
+        return setData(JSON.parse(getData));
+      }
+      return setData([]);
+    }
+  }, 0);
+
   return (
     <>
-      <input style={{width: '80%'}} type="text" placeholder='Enter a new note' />
+      <input
+        id="noteinput"
+        style={{ width: "80%" }}
+        type="text"
+        placeholder="Enter a new note"
+      />
 
       <button onClick={() => handleClick()}>Add note</button>
 
-      <Notes data={data} />      
+      <Notes data={data} />
     </>
   );
 }
